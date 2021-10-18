3c3472c2ac34f93ad2f9819c1d47b40dcc49cadf
Gabos Alpar <gabos.alpar@student.ms.sapientia.ro>
2021-10-18 16:13:23 +0300

    labor 5 orai

---
+++ lab05/Matrix.cpp
@@ -0,0 +1,159 @@
+//
+// Created by Alpar on 10/18/2021.
+//
+
+#include <ctime>
+#include "Matrix.h"
+
+Matrix::Matrix(int mRows, int mCols) {
+    this->mRows = mRows;
+    this->mCols = mCols;
+    this->mElements = new double *[mRows];
+    for (int i = 0; i < mRows; ++i) {
+        mElements[i] = new double[mCols];
+    }
+}
+
+Matrix::Matrix(const Matrix &what) {
+    mRows = what.mRows;
+    mCols = what.mCols;
+    mElements = new double *[mRows];
+    for (int i = 0; i < mRows; ++i) {
+        mElements[i] = new double[mCols];
+    }
+    for (int i = 0; i < mRows; ++i) {
+        for (int j = 0; j < mCols; ++j) {
+            mElements[i][j] = what.mElements[i][j];
+        }
+    }
+}
+
+//Matrix::Matrix(Matrix &&what) {
+//    this->mRows = what.mRows;
+//    this->mCols = what.mCols;
+//    mElements = new double *[mRows];
+//    for (int i = 0; i < mRows; ++i) {
+//        mElements[i] = new double[mCols];
+//    }
+//
+//    what.mRows = 0;
+//    what.mCols = 0;
+//    mElements = nullptr;
+//    for (int i = 0; i < mRows; ++i) {
+//        mElements[i] = nullptr;
+//    }
+//}
+
+Matrix::~Matrix() {
+    for (int i = 0; i < mRows; ++i) {
+        delete[] mElements[i];
+    }
+    delete[] mElements;
+}
+
+void Matrix::fillMatrix(double value) {
+    for (int i = 0; i < mRows; ++i) {
+        for (int j = 0; j < mCols; ++j) {
+            mElements[i][j] = value;
+        }
+    }
+}
+
+void Matrix::randomMatrix(int a, int b) {
+    srand(time(0));
+    for (int i = 0; i < mRows; ++i) {
+        for (int j = 0; j < mCols; ++j) {
+            mElements[i][j] = rand() % (b - a + 1) + a;
+        }
+    }
+}
+
+void Matrix::printMatrix(ostream &os) const {
+    for (int i = 0; i < mRows; i++)
+        for (int j = 0; j < mCols; j++)
+            // Prints ' ' if j != n-1 else prints '\n'
+            cout << mElements[i][j] << " \n"[j == mCols - 1];
+}
+
+bool Matrix::isSquare() const {
+    return mRows == mCols;
+}
+
+Matrix operator+(const Matrix &x, const Matrix &y) {
+    if (x.mRows != y.mRows || y.mCols != x.mCols) {
+        throw out_of_range("Nem megfelelo meretek!");
+    }
+    Matrix temp(x);
+
+    for (int i = 0; i < x.mRows; ++i) {
+        for (int j = 0; j < x.mCols; ++j) {
+            temp.mElements[i][j] += y.mElements[i][j];
+        }
+    }
+
+    return temp;
+}
+
+Matrix operator*(const Matrix &x, const Matrix &y) {
+    if (x.mRows != y.mCols) {
+        throw out_of_range("Nem megfelelo meretek!");
+    }
+    Matrix temp(x.mRows, y.mCols);
+
+    for (int i = 0; i < x.mRows; ++i) {
+        for (int j = 0; j < y.mCols; ++j) {
+            for (int k = 0; k < x.mCols; ++k) {
+                temp.mElements[i][j] += x.mElements[i][k] * y.mElements[k][j];
+            }
+//            temp.mElements[i][j] *= y.mElements[i][j];
+        }
+    }
+
+    return temp;
+}
+
+Matrix &Matrix::operator=(const Matrix &mat) {
+    if (this->mRows != this->mRows || mat.mCols != mat.mCols) {
+        throw out_of_range("Nem megfelelo meretek!");
+    }
+    for (int i = 0; i < mat.mRows; ++i) {
+        for (int j = 0; j < mat.mCols; ++j) {
+            this->mElements[i][j] = mat.mElements[i][j];
+        }
+    }
+}
+
+ostream &operator<<(ostream &os, const Matrix &mat) {
+    for (int i = 0; i < mat.mRows; ++i) {
+        for (int j = 0; j < mat.mCols; ++j) {
+            os << mat.mElements[i][j] << " ";
+        }
+        os << endl;
+    }
+    return os;
+}
+
+istream &operator>>(istream &is, Matrix &mat) {
+    for (int i = 0; i < mat.mRows; ++i) {
+        for (int j = 0; j < mat.mCols; ++j) {
+            is >> mat.mElements[0][j];
+        }
+    }
+
+    return is;
+}
+
+double *Matrix::operator[](int index) {
+    if (index < 0 || index >= this->mRows) {
+        throw out_of_range("Helytelen az index!");
+    }
+    return mElements[index];
+}
+
+double *Matrix::operator[](int index) const {
+    if (index < 0 || index >= this->mRows) {
+        throw out_of_range("Helytelen az index!");
+    }
+    return mElements[index];
+}
+
---
+++ lab05/main.cpp
@@ -0,0 +1,96 @@
+#include <iomanip>
+#include "Matrix.h"
+
+using namespace std;
+
+Matrix createSquareMatrix(int size) {
+    Matrix m(size, size);
+    m.fillMatrix(1);
+    return m;
+}
+
+int main() {
+    cout << "******************************************************************" << endl;
+    cout << "Constructor " << endl;
+    cout << "******************************************************************" << endl;
+    Matrix m1(2, 3);
+    m1.randomMatrix(1, 5);
+    cout << "m1: " << endl << m1 << endl;
+    cout << "******************************************************************" << endl;
+    cout << "+ operator - equal sizes " << endl;
+    cout << "******************************************************************" << endl;
+    Matrix m2(2, 3);
+    m2.fillMatrix(2);
+    cout << "m2: " << endl << m2 << endl;
+    try {
+        cout << "Matrix m3 = m1 + m2: " << endl;
+        Matrix m3 = (m1 + m2);
+        cout << "m3: " << endl << m3 << endl;
+    } catch (out_of_range &e) {
+        cout << e.what() << endl;
+    }
+    cout << "******************************************************************" << endl;
+    cout << "+ operator - different sizes " << endl;
+    cout << "******************************************************************" << endl;
+    Matrix m3(5, 5);
+    m3.fillMatrix(1);
+    cout << "m3: " << endl << m3 << endl;
+    try {
+        cout << "m1+m3:" << endl;
+        cout << "m1+m3: " << endl << m1 + m3 << endl;
+    } catch (out_of_range &e) {
+        cout << e.what() << endl;
+    }
+    cout << "******************************************************************" << endl;
+    cout << "copy assignment - different sizes " << endl;
+    cout << "******************************************************************" << endl;
+    try {
+//copy assignment
+        cout << "m3 = m1" << endl;
+        m3 = m1;
+    } catch (out_of_range &e) {
+        cout << e.what() << endl;
+    }
+    cout << "m3: " << endl;
+    cout << m3 << endl;
+    cout << "******************************************************************" << endl;
+    cout << "Extractor operator " << endl;
+    cout << "******************************************************************" << endl;
+    Matrix m4(1, 2);
+    cout << "Please type in two real numbers for m4[0][0] and m4[0][1]: " << endl;
+//Extractor operator
+    cin >> m4;
+//Inserter operator
+    cout << "m4: " << endl << m4 << endl;
+    cout<<"******************************************************************"<<endl;
+    cout<<"Index operator "<<endl;
+    cout<<"******************************************************************"<<endl;
+//Index operator
+    cout << endl << "m4[0][0]: " << m4[0][0] << endl;
+    cout<<"******************************************************************"<<endl;
+    cout<<"* operator "<<endl;
+    cout<<"******************************************************************"<<endl;
+    Matrix m5(2, 1);
+    m5.fillMatrix(1);
+    cout << "M4: " << endl << m4 << endl;
+    cout << "M5: " << endl << m5 << endl;
+    cout << "Multiplication: ";
+    try {
+        cout << "M4 x M5: " << endl << m4 * m5 << endl;
+    } catch (out_of_range& e) {
+        cout << e.what() << endl;
+    }
+    cout<<"******************************************************************"<<endl;
+    cout<<" = operator -- copy assignment "<<endl;
+    cout<<"******************************************************************"<<endl;
+    Matrix m6(m4);
+    cout << "m6 created as a copy of m4 using copy constructor: " <<endl<< m6 << endl;
+    try {
+        cout<<"m1 = m6 = m6"<<endl;
+        m1 = m6;
+    } catch (out_of_range& e) {
+        cout<< e.what() << endl;
+    }
+
+    return 0;
+}
